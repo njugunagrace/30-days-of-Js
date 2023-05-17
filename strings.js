4003ea72319a0bb20ea64912a4b84e9df910c96f
njugunagrace <njugunagraceewambui@gamil.com>
2023-05-17 07:57:09 +0300

    js

---
+++ strings.js
@@ -0,0 +1,29 @@
+let challenge = "30 days of JavaScript";
+console.log(challenge.length);
+
+let chall = challenge.toUpperCase();
+console.log(chall);
+
+let chall1 = challenge.toLowerCase();
+console.log(chall1);
+
+let slice = challenge.substring(0,2);
+console.log(slice);
+
+let slice2 = challenge.slice(3);
+console.log(slice2);
+
+let find = challenge.includes("Script");
+console.log(find);
+
+let word = challenge.split(' ');
+console.log(word);
+
+ let companies = ["facebook","google","amazon"]
+ for (let i =0; i< companies.length; i++){
+ let upper = companies[i].toUpperCase()
+ console.log(upper);
+ }
+
+ 
+
